c28a47418142ce1f99918f61f79451f56607ce89
Jhon Roly Projects <92251336+jhonrolyol@users.noreply.github.com>
2022-10-20 19:32:56 -0500

    Commit

---
+++ 01_CreacionDeUnProyecto/Module1.vb
@@ -0,0 +1,16 @@
+﻿' Link de las clases: https://www.youtube.com/playlist?list=PL7tWmaH04EfIG-ZGq4D6GlO1Pl-iNbVay
+Module Module1
+
+    Sub Main()
+        ' Preamble:
+        ' Note1.- Use "F5" for compile.
+        ' Note2.- Use "Control + S" for file save.
+
+        ' Main:
+        ' Main1.- New project
+        Console.WriteLine("Mi primera linea de comando en Visual Basic.Net")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ 02_PrimeraAplicacion/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Preamble:
+        ' Note1.- Use "F5" for compile.
+        ' Note2.- Use "Control + S" for file save.
+
+        ' Main:
+        ' Main1.- Opción 1
+        Console.WriteLine("Bienvenidos al curso de vb.net")
+        Console.ReadKey(True) ' Muestra el resultado en consola.
+
+        ' Main2.- Opción 2
+        Console.WriteLine("Hola Mundo")
+        Console.ReadLine() ' Muestra el resultado en consola.
+
+    End Sub
+
+End Module
